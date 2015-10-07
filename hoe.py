982e589f798f287d03b55209f80bf212222529fd
Sonja Rosenberger <sonja.rosenberger@rub.de>
2015-10-07 11:16:56 +0200

    Initial monograph form

--- hoe.py
+++ hoe.py
@@ -121,7 +121,7 @@ class WorkForm(Form):
         ("treaty", 'Treaty'),
         ('print', 'Print')
     ])
-
+    number_of_pages = StringField('Extent')
     genre = SelectField('Genre', choices=[
         ('legend', 'Legend'),
         ('chronicle', 'Chronicle'),
@@ -144,6 +144,14 @@ class WorkForm(Form):
 class PrintedWorkForm(WorkForm):
     publisher = StringField('Publisher')
     publisher_place = StringField('Place')
+    role = SelectField('Role', choices=[
+        ('aut', 'Author'),
+        ('edt', 'Editor'),
+        ('trl', 'Translator'),
+        ('hnr', 'Honoree'),
+        ('ive', 'Interviewee'),
+        ('ivr', 'Interviewer'),
+    ])
 
 class ManuscriptForm(WorkForm):
     incipit = TextAreaField('Incipit')
@@ -162,7 +170,7 @@ class ManuscriptForm(WorkForm):
         ('pat', 'Patron'),
         ('scr', 'Scribe'),
     ])
-    number_of_pages = StringField('Extent')
+
     number_of_lines = StringField('Number of Lines')
     library = StringField('Library')
     library_place = StringField('Library Place')
@@ -197,6 +205,9 @@ class ArticleForm(PrintedWorkForm):
         ('ivr', 'Interviewer'),
     ])
     number_of_pages = StringField('Extent')
+
+class MonographForm(PrintedWorkForm):
+    isbn = StringField('ISBN')
 
 class CollectionForm(PrintedWorkForm):
     collection_title = StringField('Collection Title', validators=[DataRequired()])
@@ -246,6 +257,19 @@ def article(primary_id=None, record_id=None):
         form.role.default = 'aut'
         form.process()
         return render_template('article_form.html', form=form, header='New Record')
+
+@app.route('/new/<primary_id>/book/<record_id>', methods=('POST',))
+@app.route('/new/<primary_id>/book', methods=('GET',))
+def book(primary_id=None, record_id=None):
+    form = MonographForm()
+    if record_id:
+        pass
+    else:
+        form.id = str(uuid.uuid4())
+        form.accessed.data = datetime.today().strftime('%Y-%m-%d')
+        form.role.default = 'aut'
+        form.process()
+        return render_template('monograph_form.html', form=form, header='New Record')
 
 @app.route('/new/<record_id>', methods=('GET', 'POST'))
 @app.route('/new', methods=('GET', 'POST'))
@@ -325,3 +349,12 @@ def new(record_id=None):
 
 if __name__ == '__main__':
     app.run()
+
+# role = SelectField('Role', choices=[
+#         ('aut', 'Author'),
+#         ('edt', 'Editor'),
+#         ('trl', 'Translator'),
+#         ('hnr', 'Honoree'),
+#         ('ive', 'Interviewee'),
+#         ('ivr', 'Interviewer'),
+#     ])
